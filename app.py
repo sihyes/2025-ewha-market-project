a2eb8f197b3c6c82992479def60b1366e69ebd74
llucyishere <lucykimlucyyy@gmail.com>
2025-11-07 13:40:24 +0900

    app.py 및 feature-list.html 수정

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import os
 from werkzeug.utils import secure_filename
 
 products = [
-    {'id': 101, 'name': '롬앤 컬러 립글로스', 'price': 9900, 'image': 'static/img/romn_gloss.jpeg'},
-    {'id': 102, 'name': '맥 립스틱', 'price': 10000, 'image': 'static/img/lipstick.jpeg'},
-    {'id': 103, 'name': '맨유 유니폼(호날두)', 'price': 70000, 'image': 'static/img/uniform.jpeg'},
-    {'id': 104, 'name': '나이키 운동화(250)', 'price': 40000, 'image': 'static/img/shoes_nike.jpeg'},
-    {'id': 105, 'name': '탁상용 선풍기', 'price': 10000, 'image': 'static/img/fan.jpeg'},
-    {'id': 106, 'name': '자라 운동화(235)', 'price': 30000, 'image': 'static/img/shoes_zara.jpeg'},
-    {'id': 107, 'name': '전공책(기본간호수기)', 'price': 5000, 'image': 'static/img/book.jpeg'},
+    {'id': 101, 'name': '롬앤 컬러 립글로스', 'price': 9900, 'image': 'img/romn_gloss.jpeg'},
+    {'id': 102, 'name': '맥 립스틱', 'price': 10000, 'image': 'img/lipstick.jpeg'},
+    {'id': 103, 'name': '맨유 유니폼(호날두)', 'price': 70000, 'image': 'img/uniform.jpeg'},
+    {'id': 104, 'name': '나이키 운동화(250)', 'price': 40000, 'image': 'img/shoes_nike.jpeg'},
+    {'id': 105, 'name': '탁상용 선풍기', 'price': 10000, 'image': 'img/fan.jpeg'},
+    {'id': 106, 'name': '자라 운동화(235)', 'price': 30000, 'image': 'img/shoes_zara.jpeg'},
+    {'id': 107, 'name': '전공책(기본간호수기)', 'price': 5000, 'image': 'img/book.jpeg'},
 ]
 
 app = Flask(__name__)
@@ -80,15 +80,12 @@ def result():
 
 @app.route('/product/<int:product_id>')
 def product_detail(product_id):
-    # product_id에 해당하는 상품 찾기
     product = next((p for p in products if p['id'] == product_id), None)
     
     if not product:
         return "해당 상품을 찾을 수 없습니다.", 404
 
-    # product-detail.html 템플릿으로 상품 데이터 전달
     return render_template('product-detail.html', product=product)
-
-    
+  
 if __name__ == '__main__':
     app.run(debug=True)
